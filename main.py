51976522b8b8a17dd7226e4bcd6e3c35f79bb66c
JustTyping1 <aditya.chorghade@outlook.com>
2022-06-23 21:51:12 +0100

    basic window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+
+blue = (0, 0, 255)
+
+window = pygame.display.set_mode((300, 300))
+
+pygame.display.set_caption("Physics simulator")
+window.fill(blue)
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():      
+        if event.type == pygame.QUIT:
+            running = False
