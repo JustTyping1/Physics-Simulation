cc4591f12032cd235b0924d4d4bbbe65d936e8bd
JustTyping1 <aditya.chorghade@outlook.com>
2022-06-24 14:23:09 +0100

    made square appear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 
+pygame.init()
+
 blue = (0, 0, 255)
+green = (0, 255, 0)
 
 window = pygame.display.set_mode((300, 300))
 
@@ -11,6 +14,11 @@ pygame.display.flip()
 running = True
 
 while running:
-    for event in pygame.event.get():      
+    for event in pygame.event.get(): 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pygame.draw.rect(window, green, pygame.Rect(30, 30, 60, 60))
+                pygame.display.flip()
+
         if event.type == pygame.QUIT:
             running = False
